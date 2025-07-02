152b4c1ce3355bf0297ca54b66b4026b5a2ed442
Caesar Moussalli <cmoussalli@gmail.com>
2025-07-02 14:43:35 +0400

    DynSettings

---
+++ CMouss.DynamicsUI/DynSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace CMouss.DynamicsUI
+{
+    public static class DynSettings
+    {
+        public static Color? NavBackgroundColor;
+        public static Color? NavBackgroundColor_Disabled;
+        public static Color? NavBackgroundColor_Hover;
+
+        public static Color? NavFontColor;
+        public static Color? NavFontColor_Disabled;
+
+
+
+
+
+    }
+}
